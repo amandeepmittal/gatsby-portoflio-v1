099ca985a7adb8cf3cbcf0a431af60eff0083ef7
amandeepmittal <amandeepmittal@live.com>
2018-01-05 13:43:10 +0530

    build 404 page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Portfolio v1`
   },
-  plugins: [`gatsby-plugin-react-helmet`],
-}
+  plugins: [`gatsby-plugin-react-helmet`]
+};
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from "react";
+import Link from "gatsby-link";
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <div style={{ color: "#e4afc7" }}>
+    <div
+      style={{
+        marginTop: "50px",
+        textAlign: "center"
+      }}
+    >
+      <h1>Oh...</h1>
+      <h2>Nothing to display here</h2>
+      <h2>
+        Please go back to{" "}
+        <Link to="/" style={{ textDecoration: "none", color: "#000" }}>
+          homepage
+        </Link>
+        !
+      </h2>
+    </div>
   </div>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
